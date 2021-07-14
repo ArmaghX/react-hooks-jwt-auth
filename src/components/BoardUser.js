d1bf10e3f14603ddee86436f95203fbe014c8d9e
ALVARO P <alv.gomez@outlook.com>
2021-07-14 13:17:37 +0200

    Fix bug on no-content components

--- src/components/BoardUser.js
+++ src/components/BoardUser.js
@@ -4,23 +4,31 @@ import { getUserBoard } from "../services/user.service";
 
 const BoardUser = () => {
   const [content, setContent] = useState("");
+  const [errorGif, setErrorGif] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
       try {
           const response = await getUserBoard();
           setContent(response.data);
+          setErrorGif("");
       } catch(error) {
           setContent(error.message);
+          setErrorGif("https://bit.ly/3kdBgVP")
       }
     }
     fetchData();
   }, []);
 
+  console.log(errorGif)
+
   return (
     <div className="container">
       <header className="jumbotron">
-        <h3>{content}</h3>
+        {errorGif ?
+          <img src={errorGif} alt="" /> :
+          <h3>{content}</h3>
+        }
       </header>
     </div>
   );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,21 +6,26 @@ const Profile = () => {
 
   return (
     <div className="container">
-      <header className="jumbotron">
-        <h3>
-          <strong>{currentUser.username}</strong> Profile
-        </h3>
-      </header>
-      <p>
-        <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
-        {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
-      </p>
-      <p>
-        <strong>Id:</strong> {currentUser.id}
-      </p>
-      <p>
-        <strong>Email:</strong> {currentUser.email}
-      </p>
+      { !currentUser ?
+      <img src="https://bit.ly/3kdBgVP" alt="" /> :
+        <>
+          <header className="jumbotron">
+            <h3>
+              <strong>{currentUser.username}</strong> Profile
+            </h3>
+          </header>
+          <p>
+            <strong>Token:</strong> {currentUser.accessToken.substring(0, 20)} ...{" "}
+            {currentUser.accessToken.substr(currentUser.accessToken.length - 20)}
+          </p>
+          <p>
+            <strong>Id:</strong> {currentUser.id}
+          </p>
+          <p>
+            <strong>Email:</strong> {currentUser.email}
+          </p>
+        </>
+      }
     </div>
   );
 };
